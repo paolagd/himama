d080a4ce23da9c7861e9d3b9967e7f325781fd16
pao <pacri14@gmail.com>
2019-02-15 19:50:03 -0500

    Update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'himama'),
-            'username' => env('DB_USERNAME', 'pmauser'),
-            'password' => env('DB_PASSWORD', 'password_here'),
+            'database' => env('DB_DATABASE', 'himama_test'),
+            'username' => env('DB_USERNAME', 'himama_tester'),
+            'password' => env('DB_PASSWORD', 'himama_t'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
